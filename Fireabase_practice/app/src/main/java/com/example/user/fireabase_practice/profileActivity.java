f8aaa60c6851d6cbd19f817d18a4f27be9f373fa
RCOMPUTER\rl3780 <rl3780@gmail.com>
2018-05-05 02:41:03 +0900

    無法push到firebase

--- Fireabase_practice/app/src/main/java/com/example/user/fireabase_practice/profileActivity.java
+++ Fireabase_practice/app/src/main/java/com/example/user/fireabase_practice/profileActivity.java
@@ -3,6 +3,7 @@ package com.example.user.fireabase_practice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -10,18 +11,22 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class profileActivity extends AppCompatActivity /*implements View.OnClickListener*/ {
 
-    /*private FirebaseAuth fa;
+    private FirebaseAuth fa;
+    /*
     private TextView userEmailTextView;
     private Button logoutButton;*/
 
     private Button send_button;
     private ListView listview;
-    private ArrayAdapter adapter;
+    private FirebaseListAdapter<ChatMessage> adapter;
     private EditText text;
 
     @Override
@@ -29,7 +34,7 @@ public class profileActivity extends AppCompatActivity /*implements View.OnClick
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        /*fa = FirebaseAuth.getInstance();
+        fa = FirebaseAuth.getInstance();
 
         if(fa.getCurrentUser() == null){
             finish();
@@ -37,7 +42,7 @@ public class profileActivity extends AppCompatActivity /*implements View.OnClick
         }
 
         FirebaseUser user = fa.getCurrentUser();
-
+        /*
         userEmailTextView = (TextView)findViewById(R.id.userEmailTextView);
         userEmailTextView.setText("Welcome " + user.getEmail());
 
@@ -46,19 +51,51 @@ public class profileActivity extends AppCompatActivity /*implements View.OnClick
         logoutButton.setOnClickListener(this);*/
         send_button = (Button)findViewById(R.id.button);
         text = (EditText)findViewById(R.id.editText);
+
         listview = (ListView) findViewById(R.id.listview);
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        displayChatMessages();
+
         send_button.setOnClickListener(send_btn);
+    }
+
+    private void displayChatMessages(){
+        FirebaseListOptions<ChatMessage> options = new FirebaseListOptions.Builder<ChatMessage>()
+                .setLayout(R.layout.message)
+                .setQuery(FirebaseDatabase.getInstance().getReference(), ChatMessage.class)
+                .build();
+        adapter = new FirebaseListAdapter<ChatMessage>(options){
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
+                // Get references to the views of message.xml
+                TextView messageText = (TextView)v.findViewById(R.id.message_text);
+                TextView messageUser = (TextView)v.findViewById(R.id.message_user);
+                TextView messageTime = (TextView)v.findViewById(R.id.message_time);
+
+                // Set their text
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+
+                // Format the date before showing it
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
+                        model.getMessageTime()));
+            }
+        };
+        listview.setAdapter(adapter);
     }
 
     private final View.OnClickListener send_btn = new View.OnClickListener() {
         @Override
         public void onClick(View btn) {
             if(text.getText().toString().length()!=0) {
-                adapter.add(text.getText().toString());
-                listview.setAdapter(adapter);
-                text.getText().clear();
-                listview.setSelection(adapter.getCount() - 1);
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new ChatMessage(text.getText().toString(),
+                                fa.getCurrentUser().getDisplayName())
+                        );
+
+                // Clear the input
+                text.setText("");
             }
         }
     };
